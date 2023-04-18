809501bd82a03929415daed38fde8104d418f481
ANSN7 <89779680+ANSN7@users.noreply.github.com>
2023-04-18 02:13:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const AWS = require("aws-sdk");
 const crypto = require("crypto");
 const fs = require("fs");
 var encryptor = require("file-encryptor");
+const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 require("dotenv").config();
-// env vars
+
 AWS.config.update({
   accessKeyId: process.env.accessKeyId,
   secretAccessKey: process.env.secretAccessKey,
@@ -14,13 +15,24 @@ AWS.config.update({
 const s3 = new AWS.S3({
   region: "us-east-1", //
 });
+
+const s3Client = new S3Client({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: "ASIATZQTYOSCMUPRMVPC",
+    secretAccessKey: "eaF3UpHKmy4jVYcz/a4E7/Tbm3UAcSdymojFLy8X",
+    sessionToken:
+      "FwoGZXIvYXdzEGIaDKH80KqiYK6nRsJs2yK7AV+PeSmGjbOaSBw6v3rQdxfZBw9jeb59ZBfScT+4jrh8UbOirMOfpCBX1lEr5P2Y5VYkdE4vk5HB8ilZxtsg0f0dJBme1eRZn5rlBx1DviNFPXIQQPT3tPze45PqQgUXWUcIZSDnbedc9DBWNJIY+/H9jVnPtUKE0KKVd6RgiU1FxITHWCkAym37tkHtI0ylzHzfEKqNGycO9GFLElkJAsIy00TMiacg+HaYCk0cVL1ds7rBS0LhlD45RKwosvX1oQYyLR61OoWmY3SKCTOofffS71DkYqGFinF3AV7x62KqaM+ic5pOzopG77a+5JeHBw==",
+  },
+  httpOptions: { timeout: 1800000 },
+});
+
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/api/s3/file/:id/upload", async (req, res) => {
-  
   var file_id = req.params["id"];
   const myBucket = "webapp1buckett";
   const downloadPath1 = "myFile200MB";
@@ -32,24 +44,9 @@ app.get("/api/s3/file/:id/upload", async (req, res) => {
     3: downloadPath3,
   };
   const path = paths[file_id];
-
-  const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
-
-  let s3Client = new S3Client({
-    region: "us-east-1",
-    credentials: {
-      accessKeyId: "ASIATZQTYOSCMUPRMVPC",
-      secretAccessKey: "eaF3UpHKmy4jVYcz/a4E7/Tbm3UAcSdymojFLy8X",
-      sessionToken:
-        "FwoGZXIvYXdzEGIaDKH80KqiYK6nRsJs2yK7AV+PeSmGjbOaSBw6v3rQdxfZBw9jeb59ZBfScT+4jrh8UbOirMOfpCBX1lEr5P2Y5VYkdE4vk5HB8ilZxtsg0f0dJBme1eRZn5rlBx1DviNFPXIQQPT3tPze45PqQgUXWUcIZSDnbedc9DBWNJIY+/H9jVnPtUKE0KKVd6RgiU1FxITHWCkAym37tkHtI0ylzHzfEKqNGycO9GFLElkJAsIy00TMiacg+HaYCk0cVL1ds7rBS0LhlD45RKwosvX1oQYyLR61OoWmY3SKCTOofffS71DkYqGFinF3AV7x62KqaM+ic5pOzopG77a+5JeHBw==",
-    },
-    httpOptions: { timeout: 1800000 },
-  });
-
   const bucketName = "webapp1buckett";
   const objectKey = path;
   let startTime = null;
-
   const oneMB = 1024 * 1024;
 
   const getObjectRange = ({ bucket, key, start, end }) => {
@@ -76,10 +73,7 @@ app.get("/api/s3/file/:id/upload", async (req, res) => {
 
   const downloadInChunks = async ({ bucket, key }) => {
     const writeStream = fs
-      .createWriteStream(
-        // fileURLToPath(new URL(`./${key}`, import.meta.url))
-        `./${path}`
-      )
+      .createWriteStream(`./${path}`)
       .on("error", (err) => console.error(err));
 
     let rangeAndLength = { start: -1, end: -1, length: -1 };
@@ -108,7 +102,6 @@ app.get("/api/s3/file/:id/upload", async (req, res) => {
       key: objectKey,
     });
 
-
     const outputFile = `encrypted_${path}.dat`;
     var key = crypto.randomBytes(32);
     var options = { algorithm: "aes256" };
@@ -129,42 +122,40 @@ app.get("/api/s3/file/:id/upload", async (req, res) => {
           throw err;
         }
 
-        const params = { Bucket: myBucket, Key: outputFile, Body: data };
+        const uploadFile = async () => {
+          const stream = fs.createReadStream(outputFile);
 
-        s3.putObject(params, function (err, data) {
-          if (err) {
-            console.log(err);
-            throw err;
-          } else {
-            const endTime = performance.now();
-            const elapsedTime = ((endTime - startTime) / 1000).toFixed(2);
-            console.log(elapsedTime, " seconds");
-            console.log("Successfully uploaded data to webapp1buckett");
-            res.send({ Elapsed_time: elapsedTime });
+          const paramsObj = {
+            Bucket: myBucket,
+            Key: outputFile,
+            Body: stream,
+            ACL: "public-read",
+          };
+
+          const options = {
+            partSize: 10 * 1024 * 1024,
+            queueSize: 1,
+          };
+
+          try {
+            await s3.upload(paramsObj, options).promise();
+            console.log("upload OK");
+          } catch (error) {
+            console.log("upload ERROR", error);
           }
+        };
+
+        uploadFile().then(() => {
+          const endTime = performance.now();
+          const elapsedTime = ((endTime - startTime) / 1000).toFixed(2);
+          console.log(elapsedTime, " seconds");
+          console.log("Successfully uploaded data to webapp1buckett");
+          res.send({ Elapsed_time: elapsedTime });
         });
       });
     });
   };
-
   main();
-  // console.log("Download completed");
-
-  // rs.on("error", (err) => {
-  //   // file.destroy();
-  //   console.error("Download failed:", err);
-  // });
-
-  // res.attachment(path);
-  // var fileStream = s3.getObject({ Bucket: myBucket, Key: path }).createReadStream();
-  // fileStream.pipe(res);
-
-  // const fileStream = s3
-  //   .getObject({ Bucket: myBucket, Key: path })
-  //   .createReadStream();
-  // // Download the file from S3 and save it to the file system
-  // const file = fs.createWriteStream("file");
-  // fileStream.pipe(file);
 });
 app.timeout = 0;
 app.listen(PORT);
